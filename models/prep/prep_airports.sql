ebb669414f38d9bb5edde305f368ef1408f974de
Starryzhan1 <starryzlq@gmail.com>
2025-07-15 16:09:37 +0200

    table updated

--- models/prep/prep_airports.sql
+++ models/prep/prep_airports.sql
@@ -2,6 +2,13 @@ WITH airports_reorder AS (
         SELECT faa
         	   ,country
         	   ,region
+               ,name
+               ,lat
+               ,lon
+               ,alt
+               ,tz
+               ,dst
+               ,city
         FROM {{ref('staging_airports')}}
     )
     SELECT * FROM airports_reorder
