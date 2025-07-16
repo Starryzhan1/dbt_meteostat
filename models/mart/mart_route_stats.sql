c234ed0c806fc4a15ba06949b6c32b818af5f3af
Starryzhan1 <starryzlq@gmail.com>
2025-07-16 13:57:03 +0200

    table added

--- models/mart/mart_route_stats.sql
+++ models/mart/mart_route_stats.sql
@@ -1,7 +1,7 @@
 WITH route_table AS 
 (SELECT origin, dest, count(DISTINCT tail_number) AS unique_tail_number, count(DISTINCT airline) AS unique_airline, count(flight_number) AS total_flight,
        round(avg(actual_elapsed_time),2) AS avg_actual_elapsed_time, round(avg(arr_delay),2) AS avg_arr_delay,Max(dep_delay+arr_delay) AS Max_delay, Min(dep_delay+arr_delay) AS Min_delay, sum(cancelled) AS total_cancelled, sum(diverted) AS total_diverted
-      FROM {{ref('prep_flights pf')}}
+      FROM {{ref('prep_flights')}} as pf
       GROUP BY origin, dest
 ),
 route_stats AS (
@@ -13,10 +13,9 @@ route_stats AS (
 	       pa2.name AS dest_name,
 	       rt.* 
 	FROM route_table rt
-	JOIN {{ref('prep_airports pa')}}
+	JOIN {{ref('prep_airports')}} as pa
 	ON  rt.origin = pa.faa 
-	JOIN prep_airports pa2
+	JOIN {{ref('prep_airports')}} as pa2
 	ON rt.dest = pa2.faa
 )
 SELECT * FROM route_stats
-       
